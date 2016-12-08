b146c95c80b26192844813f78f8ff0313ff6cfed
Luke Go <luke.go@hardkernel.com>
2016-12-08 16:50:57 +0900

    Revert "PD#105949 support instaboot"
    Conflicts:
    	recovery.cpp
    	roots.h
    
    This reverts commit 6fa14bc26b9fa8665f8c48f8e16a85303ca8bbc7.
    
    Change-Id: I88d1934d73f2c7797adfe31f3e43b8fd1c8e2ddc

--- roots.h
+++ roots.h
@@ -46,10 +46,6 @@ int format_volume(const char* volume);
 // mounted (/tmp and /cache) are mounted.  Returns 0 on success.
 int setup_install_mounts();
 
-//clear instaboot image
-int instaboot_clear();
-int instaboot_disable();
-
 #ifdef __cplusplus
 }
 #endif
