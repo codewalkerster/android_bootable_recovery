33da2cf02d2bada971c07b66b45e920ba3cba8f1
Chris <codewalker@hardkernel.com>
2021-08-27 10:48:25 +0900

    Revert "pcba: support display rgb"
    This reverts commit 6a2691ea76d6ed3ad612f51653c655e687cbb7a5.
    
    Change-Id: I49f417b07ac118a5bfb6098e10ca755c94444f97

--- recovery_ui/include/recovery_ui/ui.h
+++ recovery_ui/include/recovery_ui/ui.h
@@ -189,10 +189,6 @@ class RecoveryUI {
   void SetEnableFastbootdLogo(bool enable) {
     fastbootd_logo_enabled_ = enable;
   }
-  // Set whether or not the RGA in PCBA is displayed.
-  void SetRkFactoryStart(bool enable) {
-    RkFactory_Start_ = enable;
-  }
 
   // Resets the key interrupt status.
   void ResetKeyInterruptStatus() {
@@ -230,7 +226,6 @@ class RecoveryUI {
   bool touch_finger_down_;
 
   bool fastbootd_logo_enabled_;
-  bool RkFactory_Start_;
 
  private:
   enum class ScreensaverState {
